2f99cf70f81ea1411bafc9d892134bb31d63b917
Oliver <olliebrownlow@gmail.com>
2019-07-01 16:19:41 +0100

    does a rubocop and account.rb refactor

--- lib/account.rb
+++ lib/account.rb
@@ -1,20 +1,24 @@
 class Account
-	attr_reader :balance, :transactions
+	 attr_reader :balance, :transactions
 
-	def initialize
-		@balance = 0
-		@transactions = []
-	end
+	 def initialize
+ 		 @balance = 0
+ 		 @transactions = []
+ 	end
 
-	def deposit(amount)
-		@balance += amount
-		formatted_date = Time.now.strftime("%d/%m/%Y")
-		@transactions.push([formatted_date, amount, "", @balance])
-	end
+	 def deposit(amount)
+ 		 @balance += amount
+ 		 @transactions.push([date_format, amount, "", @balance])
+ 	end
 
-	def withdraw(amount)
-		@balance -= amount
-		formatted_date = Time.now.strftime("%d/%m/%Y")
-		@transactions.push([formatted_date, "", amount, @balance])
-	end
+	 def withdraw(amount)
+ 		 @balance -= amount
+ 		 @transactions.push([date_format, "", amount, @balance])
+ 	end
+
+	private
+
+	 def date_format
+ 		 Time.now.strftime("%d/%m/%Y")
+ 	end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,39 +4,39 @@ describe Account do
   before(:each) { @account = Account.new }
 
   describe "a new account" do
-		it "starts with a balance of 0" do
-			expect(@account.balance).to eq(0)
-		end
+		  it "starts with a balance of 0" do
+  			 expect(@account.balance).to eq(0)
+  		end
 		
-		it "starts with an empty array of transactions" do
-			expect(@account.transactions).to eq([])
-		end 
+		  it "starts with an empty array of transactions" do
+  			 expect(@account.transactions).to eq([])
+  		end 
 
-	end
+	 end
 
-	describe "making deposits and withdrawals" do
-		it "adds the expressed deposit amount to the balance" do
-			@account.deposit(100)
-			expect(@account.balance).to eq(100)
-		end
+	 describe "making deposits and withdrawals" do
+ 		 it "adds the expressed deposit amount to the balance" do
+  			 @account.deposit(100)
+  			 expect(@account.balance).to eq(100)
+  		end
 
-		it "debits the expressed withdrawal amount from the balance" do
-			@account.deposit(500)
-			@account.withdraw(100)
-			expect(@account.balance).to eq(400)
-		end
-	end
+ 		 it "debits the expressed withdrawal amount from the balance" do
+  			 @account.deposit(500)
+  			 @account.withdraw(100)
+  			 expect(@account.balance).to eq(400)
+  		end
+ 	end
 
-	describe "storing transactions" do
-		it "stores each deposit transaction with a date stamp, the deposit amount, an empty string for withdrawals and the balance" do
-			@account.deposit(500)
-			expect(@account.transactions).to include(["01/07/2019", 500, "", @account.balance])
-		end
+	 describe "storing transactions" do
+ 		 it "stores each deposit transaction with a date stamp, the deposit amount, an empty string for withdrawals and the balance" do
+  			 @account.deposit(500)
+  			 expect(@account.transactions).to include(["01/07/2019", 500, "", @account.balance])
+  		end
 
-		it "stores each withdrawal transaction with a date stamp, an empty string for deposits, the withdrawal amount and the balance" do
-			@account.deposit(500)
-			@account.withdraw(200)
-			expect(@account.transactions).to include(["01/07/2019", "", 200, @account.balance])
-		end
-	end
+ 		 it "stores each withdrawal transaction with a date stamp, an empty string for deposits, the withdrawal amount and the balance" do
+  			 @account.deposit(500)
+  			 @account.withdraw(200)
+  			 expect(@account.transactions).to include(["01/07/2019", "", 200, @account.balance])
+  		end
+ 	end
 end
